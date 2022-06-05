426a307c6d907502569ff71394cf77a565bba413
rbenitec <105337433+rbenitec@users.noreply.github.com>
2022-06-04 23:31:17 -0500

    update

---
+++ src/main/java/NTTDATA/msclient/config/ClientWebClient.java
@@ -0,0 +1,17 @@
+package NTTDATA.msclient.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@Getter
+@Setter
+public class ClientWebClient {
+    private final WebClient webClient;
+
+    public ClientWebClient(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("http://service-currentaccounts/current").build();
+    }
+}
--- src/main/java/NTTDATA/msclient/service/implemt/OperationServiceImp.java
+++ src/main/java/NTTDATA/msclient/service/implemt/OperationServiceImp.java
@@ -1,5 +1,6 @@
 package NTTDATA.msclient.service.implemt;
 
+import NTTDATA.msclient.config.ClientWebClient;
 import NTTDATA.msclient.model.CurrentAccount;
 import NTTDATA.msclient.service.OperationService;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,25 @@ public class OperationServiceImp implements OperationService {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    ClientWebClient clientWebClient;
+
+    @Override
+    public Flux<CurrentAccount> getCurrentAccounts(String clientId) {
+        //return clientWebClient.getWebClient().get().uri("/byclient/"+clientId);
+        return (Flux<CurrentAccount>) clientWebClient.getWebClient().get().uri("/byclient/"+clientId);
+    }
+
+    /*
     @Override
     public Flux<CurrentAccount> getCurrentAccounts(String clientId) {
         List<CurrentAccount> currentAccounts = restTemplate.getForObject("http://service-currentaccounts/current/byclient/"+clientId, List.class);
         //Flux<CurrentAccount> currentAccount = restTemplate.getForObject("http://service-currentaccounts/current/byclient/"+clientId, Flux.class);
         return Flux.fromIterable(currentAccounts);
     }
+    */
+
+    //Aplicando el Web client para obtener una cuenta por codigo del cliente
+
+
 }
